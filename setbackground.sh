883f3601351bc2d6509769db4b45059acd8be15e
berock212 <brockers212@hotmail.com>
2016-01-31 21:02:31 -0500

    Bug fixes

--- setbackground.sh
+++ setbackground.sh
@@ -3,6 +3,7 @@
 #script to set the desktop background to a randomly generated maze
 
 #location of mazepng
+
 prog=~/documents/git/Maze-background/mazepng
 
 if [ ! -a maze.png ]; then
@@ -20,14 +21,14 @@ colmult=$RANDOM
 
 
 let "number %= $ODDS"
-let "colmult %=100"
+let "colmult %=5"
 if [ "$number" -eq $T ]
 then
-$prog -w 1920 -h 1080 -p 20 -b 1 -n maze.png -t $colmult  
+$prog -w 1920 -h 1080 -p 1 -b 0 -t $colmult -n maze.png 
 else
-$prog -w 1920 -h 1080 -p 1 -b 0 -n maze.png 
+$prog -w 1920 -h 1080 -p 20 -b 1 -n maze.png   
 fi  
 
 #time till it generates a new background
-sleep 600;
+sleep 10;
 done
